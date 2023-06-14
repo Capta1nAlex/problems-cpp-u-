f30d9af1257b9f6ab24cb6ac51bb2996a265ffce
Oleksii Morozov <91106292+Capta1nAlex@users.noreply.github.com>
2023-06-14 22:50:18 +0200

    Update luckyTicket.cpp
    added comments, so it's easier to read the code

--- e-olymp/lucky tickets/luckyTicket.cpp
+++ e-olymp/lucky tickets/luckyTicket.cpp
@@ -5,13 +5,13 @@
 using namespace std;
 int main()
 { 
-	int n;
+	int n; // sum of the three digits of lucky tickets
 	cin >> n;
-	int luckycount = 0;
-	for (int i = 0; i < 1000; i++) {
+	int luckycount = 0; // amount of combinations on 1 side
+	for (int i = 0; i < 1000; i++) { // ticket numbers are [000,001...,999]
 		if (i / 100 + i / 10 % 10 + i % 10 == n) {
 			luckycount++;
 		}
 	}
-	cout << pow(luckycount, 2);
+	cout << pow(luckycount, 2); 
 }
