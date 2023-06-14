e15500bd0936309f07308beedd2fcfb14b3d871b
Oleksii Morozov <91106292+Capta1nAlex@users.noreply.github.com>
2023-06-14 22:59:15 +0200

    Update Meteo measurements.cpp
    slightly changed code and added comments.

--- e-olymp/meteo measurements/Meteo measurements.cpp
+++ e-olymp/meteo measurements/Meteo measurements.cpp
@@ -4,31 +4,30 @@
 using namespace std;
 int main()
 {
-	int n, k;
-	double min, max;
+	int n, k; // n and k - the number of temperature measurements and the number of days to calculate the average temperature
+	double min, max; 
 	cin >> n >> k;
 	vector <int> vec;
-	double temp;
+	double input, avg = 0;
 	for (int i = 0; i < n; i++) {
-		cin >> temp;
-		vec.push_back(temp);
+		cin >> input;
+		vec.push_back(input);
 	}
-	temp = 0;
-	for (int i = 0; i < k; i++) {
-		temp += vec[i];
+	for (int i = 0; i < k; i++) { 
+		avg += vec[i];
 	}
-	min = temp/k;
-	max = temp/k;
+	min = avg/k; // setting min as an average from first k days
+	max = avg/k; // setting max as an average from first k days
 
 	for (int i = 1; i <= n-k; i++)
 	{
-		temp += vec[i + k-1];
-		temp -= vec[i - 1];
-		if (temp/k > max) {
-			max = temp/k;
+		avg += vec[i + k-1]; // adding next day to the calculation 
+		avg -= vec[i - 1]; // removing previous day from the calculation
+		if (avg/k > max) { 
+			max = avg/k;
 		}
-		else if (temp/k < min) {
-			min = temp/k;
+		else if (avg/k < min) {
+			min = avg/k;
 		}
 	}
 	cout << round(min) << endl << round(max);
