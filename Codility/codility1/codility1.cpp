e85ba17700af5a8e03065ff8c2c48ceffe67c0f2
Oleksii Morozov <91106292+Capta1nAlex@users.noreply.github.com>
2023-06-15 00:18:18 +0200

    Update codility1.cpp
    added comments, so it's easier to read the code

--- Codility/codility1/codility1.cpp
+++ Codility/codility1/codility1.cpp
@@ -10,20 +10,20 @@ int main()
 	int binaryNum[32];
 	cin >> N;
 	int count = 0;
-	while (N > 0) {
-		binaryNum[count] = N % 2;
+	while (N > 0) { //creating an array that will represent a binary number
+		binaryNum[count] = N % 2; 
 		N /= 2;
 		count++;
 	}
-	for (int i = count; i > 0; i--) {
-		if (binaryNum[i] == 0) {
+	for (int i = count; i > 0; i--) {  
+		if (binaryNum[i] == 0) { // if there's a 0 in between of ones increase by 1 
 			tempSum++;
 		}
-		else {
+		else { 
 			if (tempSum > maxSum) {
 				maxSum = tempSum;
 			}
-			tempSum = 0;
+			tempSum = 0; // reset zeros count
 		}
 	}
 	cout << maxSum;
