d0bceb4c446eb8de7e77ced07e33fcda273cb5ca
Oleksii Morozov <91106292+Capta1nAlex@users.noreply.github.com>
2023-06-14 22:43:51 +0200

    Update Corruption.cpp
    added comments, so it's easier to read the code

--- e-olymp/corruption/Corruption.cpp
+++ e-olymp/corruption/Corruption.cpp
@@ -6,24 +6,23 @@
 using namespace std;
 int main()
 {
-	int n, p, temp;
+	int n, p, tempInput; // N is amount of accounts, P is a fee for merging. 
 	cin >> n >> p;
-	p = 100 - p;
-	multiset<long long> c;
+	p = 100 - p; // Value-(Value*p/100) is the same as Value*(100-p)/100, but second variant is a little bit easier to read
+	multiset<long long> c; 
 	for (int i = 0; i < n; i++) {
-		cin >> temp;
-		c.insert(temp * 100);		
+		cin >> tempInput;
+		c.insert(tempInput * 100); // In this task we don't round money that is less than 0.01 UAH, because of it I'm keeping values in int till the end, not in double
 	}
 	int newElement;    
 	multiset<long long>::iterator ptr1;
 	ptr1 = c.begin();
 	for (int i = n; n > 1; n--) {
-		newElement = (*next(c.begin(), 0) + *next(c.begin(), 1)) * p / 100;
-		c.insert(newElement);
-		ptr1++;
-		ptr1++;
-		c.erase(c.begin(), ptr1);
+		newElement = (*next(c.begin(), 0) + *next(c.begin(), 1)) * p / 100; 
+		c.insert(newElement); // Inserting a new merged account into a set
+		ptr1+=2; 
+		c.erase(c.begin(), ptr1); // we use iterator in order to clear previous 2 bank accounts
 	}
-	double result = double(newElement) / 100;
+	double result = double(newElement) / 100; // final value is double, because we should show amount of coins on balance, for example "4150.50"
 	cout << result << endl;
 }
