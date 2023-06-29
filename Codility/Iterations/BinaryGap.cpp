8602cbb441959b73682a4d22d3fc835b140a4045
Oleksii Morozov <91106292+Capta1nAlex@users.noreply.github.com>
2023-06-29 04:54:57 +0200

    Update BinaryGap.cpp
    Removed code from main and added it as a function

--- Codility/Iterations/BinaryGap.cpp
+++ Codility/Iterations/BinaryGap.cpp
@@ -1,30 +1,22 @@
-// codility1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-using namespace std;
-int main()
-{
-	int N;
+int solution(int N) {
 	int maxSum = 0, tempSum = 0;
 	int binaryNum[32];
-	cin >> N;
 	int count = 0;
-	while (N > 0) { //creating an array that will represent a binary number
-		binaryNum[count] = N % 2; 
+	while (N > 0) {
+		binaryNum[count] = N % 2;
 		N /= 2;
 		count++;
 	}
-	for (int i = count; i > 0; i--) {  
-		if (binaryNum[i] == 0) { // if there's a 0 in between of ones increase by 1 
+	for (int i = count-1; i >= 0; i--) {
+		if (binaryNum[i] == 0) {
 			tempSum++;
 		}
-		else { 
-			if (tempSum > maxSum) {
+		else {
+			if (tempSum >= maxSum) {
 				maxSum = tempSum;
 			}
-			tempSum = 0; // reset zeros count
+			tempSum = 0;
 		}
 	}
-	cout << maxSum;
+    return maxSum;
 }
