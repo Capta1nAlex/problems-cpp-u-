6081e4f51151abadb4cb776479334672adeab15e
Oleksii Morozov <91106292+Capta1nAlex@users.noreply.github.com>
2023-07-14 09:01:14 +0200

    Add files via upload

---
+++ e-olymp/billiard/Billiard.cpp
@@ -0,0 +1,41 @@
+// Billiard.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std; 
+
+int gcd(int a, int b) {
+	if (a == 0) return b;
+	return gcd(b % a, a);
+}
+
+
+int main()
+{
+	long x, y;
+	cin >> x >> y;
+	long dividor = gcd(x, y);
+	long long hits = x / dividor + y / dividor - 2;
+	cout << hits << " ";
+	if (hits % 2 == 0 || hits == 0) {
+		cout << 3;
+	}
+	else if ((x / dividor) % 2 == 0) {
+		cout << 2;
+	}
+	else {
+		cout << 4;
+	}
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
